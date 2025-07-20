d800e4a8780481034f7963f1be2fa7c8ae8c9dde
Kellan Barrett <kellanjack@gmail.com>
2025-07-19 22:02:44 -0500

    Change Public to public in JSDoc comments

--- server/routes/itemRoutes.js
+++ server/routes/itemRoutes.js
@@ -5,35 +5,35 @@ const itemController = require('../controllers/itemController');
 /**
  * @route   GET /api/items
  * @desc    Get all items with optional filtering
- * @access  Public
+ * @access  public
  */
 router.get('/', itemController.getAllItems);
 
 /**
  * @route   GET /api/items/category/:category
  * @desc    Get items by category
- * @access  Public
+ * @access  public
  */
 router.get('/category/:category', itemController.getItemsByCategory);
 
 /**
  * @route   GET /api/items/search
  * @desc    Search items by name or description
- * @access  Public
+ * @access  public
  */
 router.get('/search', itemController.searchItems);
 
 /**
  * @route   GET /api/items/:id
  * @desc    Get single item by ID
- * @access  Public
+ * @access  public
  */
 router.get('/:id', itemController.getItemById);
 
 /**
  * @route   GET /api/items/categories
  * @desc    Get all available categories
- * @access  Public
+ * @access  public
  */
 router.get('/categories', itemController.getCategories);
 
